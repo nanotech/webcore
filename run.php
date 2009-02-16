503199df5afc4db341b90a0ea947c858d7fbc734
NanoTech <nanotech008@gmail.com>
2009-02-15 19:20:54 -0600

    Improved setup script.

--- run.php
+++ run.php
@@ -4,17 +4,17 @@ if (!defined('APP_DIR') || APP_DIR === false) die('Please define APP_DIR.');
 
 set_include_path(get_include_path().'.:'.APP_DIR.':'.WEBCORE_DIR.'/default:');
 
-require 'webcore/Core.php';
+require WEBCORE_DIR.'/Core.php';
 require 'config/constants.php';
 
 Core::index_resources(array(
 	'php' => array(
+		WEBCORE_DIR.'/default/app/controllers',
+		WEBCORE_DIR.'/default/app/plugins',
+		APP_DIR.'/controllers',
+		APP_DIR.'/models',
+		APP_DIR.'/plugins',
 		WEBCORE_DIR,
-		APP_DIR.'/default/controllers',
-		APP_DIR.'/default/models',
-		APP_DIR.'/default/plugins',
-		WEBCORE_DIR.'/default/controllers',
-		WEBCORE_DIR.'/default/plugins',
 	),
 	'views' => array(
 		APP_DIR.'/views',
--- script/setup.php
+++ script/setup.php
@@ -1,100 +1,109 @@
+#!/usr/bin/env php
 <?php
 /**
  * Copies base files from webcore/default into app
  */
 
-$base_path = rtrim(dirname(__FILE__), '/script');
-$base_url = rtrim(dirname($_SERVER['REQUEST_URI']), '/script').'/';
+define('IN_TERMINAL', !isset($_SERVER['REQUEST_URI']));
+define('WEBCORE_DIR', substr(__FILE__, 0, -strlen('/script/setup.php')));
+define('WEBCORE_DEFAULTS', WEBCORE_DIR.'/default');
 
-$defaults = 'webcore/default';
-$app = $base_path.'/app';
-chdir($base_path);
-
-if(!file_exists('cache')) {
-	mkdir('cache');
+if (IN_TERMINAL) {
+	$appname = $argv[1];
+	if (isset($appname) && !empty($appname)) {
+		define('BASE_DIR', getcwd().'/'.$appname);
+		define('BASE_URI', $argv[2]);
+	} else {
+		say('Please specify a name for the app.');
+		exit;
+	}
+} else {
+	define('BASE_DIR', rtrim(dirname(__FILE__), '/script'));
+	define('BASE_URI', rtrim(dirname($_SERVER['REQUEST_URI']), '/script').'/');
+	chdir(BASE_DIR);
+	say('Please run this script from the command line');
 }
 
-if(!file_exists($base_path.'/app')) {
-	mkdir($app);
-	mkdir($app.'/plugins');
-	copy_r($defaults.'/config', $app.'/config');
-	copy_r($defaults.'/views', $app.'/views');
-	copy_r($defaults.'/controllers', $app.'/controllers');
-	copy($defaults.'/index.php', $base_path.'/index.php');
+define('APP_DIR', BASE_DIR.'/app');
 
-	$htaccess = file_get_contents($defaults.'/htaccess');
-	$htaccess =
-		str_replace('RewriteBase "/"', "RewriteBase '$base_url'", $htaccess);
+if (!file_exists(BASE_DIR)) {
+	mkdir(BASE_DIR);
+	mkdir(APP_DIR);
+	mkdir(APP_DIR.'/plugins');
+	mkdir(APP_DIR.'/cache');
+	chmod(APP_DIR.'/cache', 0777);
+	say('Copying defaults...');
+	copy_defaults('app/config');
+	copy_defaults('app/views');
+	copy_defaults('app/controllers');
+	copy_defaults('static');
+	copy_default_and_modify('htaccess', 'RewriteBase /', 'RewriteBase '.BASE_URI.'/', '.htaccess');
+	copy_default_and_modify('index.php', 'webcore/run.php', WEBCORE_DIR.'/run.php');
+	copy_default_and_modify('app/config/constants.php', "define('BASE_URL', '/')", "define('BASE_URL', '".BASE_URI."/')");
 
-	file_put_contents($base_path.'/.htaccess', $htaccess, FILE_APPEND);
-
-	echo 'app created';
+	say('App created.');
 
 } else {
-	echo 'app already exists!';
+	say('App already exists!');
 }
 
-echo "\n";
+function say($string) {
+	echo $string."\n";
+}
 
-# from http://php.net/copy
-function copy_r($srcdir, $dstdir, $offset = '', $verbose = false)
+function copy_defaults($what, $to=BASE_DIR, $from=WEBCORE_DEFAULTS)
 {
-    // A function to copy files from one directory to another one, including subdirectories and
-    // nonexisting or newer files. Function returns number of files copied.
-    // This function is PHP implementation of Windows xcopy  A:\dir1\* B:\dir2 /D /E /F /H /R /Y
-    // Syntaxis: [$returnstring =] dircopy($sourcedirectory, $destinationdirectory [, $offset] [, $verbose]);
-    // Example: $num = dircopy('A:\dir1', 'B:\dir2', 1);
+	$from = $from.'/'.$what;
+	$to = $to.'/'.$what;
+	copy_r($from, $to);
+}
 
-    // Original by SkyEye.  Remake by AngelKiha.
-    // Linux compatibility by marajax.
-    // Offset count added for the possibilty that it somehow miscounts your files.  This is NOT required.
-    // Remake returns an explodable string with comma differentiables, in the order of:
-    // Number copied files, Number of files which failed to copy, Total size (in bytes) of the copied files,
-    // and the files which fail to copy.  Example: 5,2,150000,\SOMEPATH\SOMEFILE.EXT|\SOMEPATH\SOMEOTHERFILE.EXT
-    // If you feel adventurous, or have an error reporting system that can log the failed copy files, they can be
-    // exploded using the | differentiable, after exploding the result string.
-    //
-    if(!isset($offset)) $offset=0;
-    $num = 0;
-    $fail = 0;
-    $sizetotal = 0;
-    $fifail = '';
-    if(!is_dir($dstdir)) mkdir($dstdir);
-    if($curdir = opendir($srcdir)) {
-        while($file = readdir($curdir)) {
-            if($file != '.' && $file != '..') {
-                $srcfile = $srcdir . '/' . $file;
-                $dstfile = $dstdir . '/' . $file;
-                if(is_file($srcfile)) {
-                    if(is_file($dstfile)) $ow = filemtime($srcfile) - filemtime($dstfile); else $ow = 1;
-                    if($ow > 0) {
-                        if($verbose) echo "Copying '$srcfile' to '$dstfile'...<br />";
-                        if(copy($srcfile, $dstfile)) {
-                            touch($dstfile, filemtime($srcfile)); $num++;
-                            chmod($dstfile, 0777);    # added by marajax
-                            $sizetotal = ($sizetotal + filesize($dstfile));
-                            if($verbose) echo "OK\n";
-                        }
-                        else {
-                            echo "Error: File '$srcfile' could not be copied!<br />\n";
-                            $fail++;
-                            $fifail = $fifail.$srcfile.'|';
-                        }
-                    }
-                }
-                else if(is_dir($srcfile)) {
-                    $res = explode(',',$ret);
-                    $ret = copy_r($srcfile, $dstfile, $verbose);
-                    $mod = explode(',',$ret);
-                    $imp = array($res[0] + $mod[0],$mod[1] + $res[1],$mod[2] + $res[2],$mod[3].$res[3]);
-                    $ret = implode(',',$imp);
-                }
-            }
-        }
-        closedir($curdir);
-    }
-    $red = explode(',',$ret);
-    $ret = ($num + $red[0]).','.(($fail-$offset) + $red[1]).','.($sizetotal + $red[2]).','.$fifail.$red[3];
-    return $ret;
+function copy_default_and_modify($what, $find, $replace, $what_new=false, $to=BASE_DIR, $from=WEBCORE_DEFAULTS)
+{
+	if ($what_new == false) $what_new = $what;
+	$file = file_get_contents($from.'/'.$what);
+	$file = str_replace($find, $replace, $file);
+	file_put_contents($to.'/'.$what_new, $file);
+}
+
+/**
+ * Copies entire directories recursively,
+ * overwriting existing files.
+ *
+ * Authors: SkyEye, AngelKiha, marajax, NanoTech.
+ * Original from <http://php.net/copy>.
+ */
+function copy_r($srcdir, $dstdir, $ignore_dotfiles=true)
+{
+	if (!is_dir($srcdir)) {
+		return copy($srcdir, $dstdir);
+	}
+
+	if (!is_dir($dstdir)) mkdir($dstdir);
+	if ($curdir = opendir($srcdir))
+	{
+		while ($file = readdir($curdir))
+		{
+			if (($ignore_dotfiles && $file{0} == '.') || $file == '.' || $file == '..') {
+				continue;
+			}
+
+			$srcfile = $srcdir.'/'.$file;
+			$dstfile = $dstdir.'/'.$file;
+
+			if (is_file($srcfile)) {
+				if (!copy($srcfile, $dstfile)) {
+					echo "Error: File '$srcfile' could not be copied!\n";
+				}
+
+			} elseif (is_dir($srcfile)) {
+				copy_r($srcfile, $dstfile, $ignore_dotfiles);
+			}
+		}
+
+		closedir($curdir);
+	}
+
+	return true;
 }
 ?>
