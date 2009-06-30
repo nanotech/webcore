836416f04c851896df32c41a81b01414c97cb591
NanoTech <nanotech008@gmail.com>
2009-06-30 15:23:51 -0600

    Layout: Allow excluding the $filters parameter.

--- default/app/plugins/Layout.php
+++ default/app/plugins/Layout.php
@@ -7,7 +7,7 @@ class Layout extends Filter {
 
 	static public $type = 'a -> b';
 
-	public function parse($data, $file, $filters, $content_var='the_content')
+	public function parse($data, $file, $filters=array(), $content_var='the_content')
 	{
 		$display = new Display((array) $filters);
 		$display->meta = $this->meta;
